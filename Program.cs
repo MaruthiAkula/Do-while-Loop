825a82165d55f6f53723e2d596a458ee97a4cc7d
Maruthi Akula <maruthiraoakula22@gmail.com>
2022-07-03 10:40:22 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_while_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Userchoice = "";
+            do
+            {
+                int count = 0;
+                int start = 0;
+                Console.WriteLine("Enter Your Target");
+                int Usertarget = int.Parse(Console.ReadLine());
+                while (start <= Usertarget)
+                {
+                    Console.WriteLine(start + "");
+                    start = start + 2;
+                    count++;
+
+                }
+                Console.WriteLine("count= {0}", count);
+                do
+                {
+                    Console.WriteLine("do you want to continue yes or no");
+                    Userchoice = Console.ReadLine().ToUpper();
+                    if (Userchoice != "YES" && Userchoice != "NO")
+                    {
+                        Console.WriteLine("Userchoice Invalid Please enter Yes or No");
+                    }
+                } while (Userchoice != "YES" && Userchoice != "NO");
+            } while (Userchoice == "YES");
+        }
+    }
+}
